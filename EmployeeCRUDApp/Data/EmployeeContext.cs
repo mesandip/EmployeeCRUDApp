5b40ace30ecef36d5a5653fe1dc004cfbdefc1ae
Admin <Admin@DESKTOP-MK2UQG5>
2023-03-14 14:40:05 +0530

    Add project files.

---
+++ EmployeeCRUDApp/Data/EmployeeContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EmployeeCRUDApp.Models;
+
+namespace EmployeeCRUDApp.Data
+{
+    public class EmployeeContext :DbContext
+    {
+        public EmployeeContext(DbContextOptions <EmployeeContext> options) :base (options) { }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ EmployeeCRUDApp/Startup.cs
@@ -0,0 +1,40 @@
+﻿using EmployeeCRUDApp.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Configuration;
+
+
+namespace EmployeeCRUDApp
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        { Configuration = configuration; }
+
+        public void Configure(IApplicationBuilder app)
+        {
+
+
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddDbContext<EmployeeContext>(item => item.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            //services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+    }
+}
